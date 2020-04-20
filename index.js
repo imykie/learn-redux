60fc01561c74529bc8e7ca9729f0e85dad391a73
Michael Olatunji <tundexmike@gmail.com>
2020-04-20 17:11:46 +0100

    basic redux pattern

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log('Learn redux')
+const redux = require('redux');
+
+const { createStore } = redux
+
+const BUY_CAKE = 'BUY_CAKES'
+
+function buyCake(){
+    return {
+        type: BUY_CAKE,
+        info: 'First redux action'
+    }
+}
+
+// (previousState, action) => newState
+
+const initialState = {
+    numOfCakes: 10
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        }
+
+        default: return state
+    }
+}
+
+const store = createStore(reducer)
+
+console.log('initial state', store.getState());
+
+const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()))
+
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+
+unsubscribe();
